f8ecc2e32f2ca75eb1e4c5563873d9ccb12471bb
AntanasZ <D00218148@student.dkit.ie>
2022-05-07 13:03:32 +0100

    Added Background Texture
    -Added background texture from CA2

--- RoboCatSFMLClient/TextureManager.cpp
+++ RoboCatSFMLClient/TextureManager.cpp
@@ -12,6 +12,7 @@ TextureManager::TextureManager()
 	CacheTexture("cat", "../Assets/cat.png");
 	CacheTexture("mouse", "../Assets/mouse.png");
 	CacheTexture("yarn", "../Assets/yarn.png");
+	CacheTexture("background", "../Assets/Background.png");
 }
 
 TexturePtr	TextureManager::GetTexture(const string& inTextureName)
