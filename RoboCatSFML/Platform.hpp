4447742a0c5dc474abda021f7235dbb88b80d9d3
AntanasZ <D00218148@student.dkit.ie>
2022-05-08 13:01:41 +0100

    Set up Platforms and Background
    -Set up platforms around the game
    -Edited ground platform and background textures to fit with new game size

--- RoboCatSFML/Platform.hpp
+++ RoboCatSFML/Platform.hpp
@@ -3,8 +3,21 @@ class Platform : public GameObject
 public:
 	CLASS_IDENTIFICATION('PLAT', GameObject)
 
+	enum EPlatformReplicationState
+	{
+		EMRS_Pose = 1 << 0,
+		EMRS_Color = 1 << 1,
+
+		EMRS_AllState = EMRS_Pose | EMRS_Color
+	};
+
 	static GameObject* StaticCreate() { return new Platform(); }
 	virtual bool HandleCollisionWithCat(RoboCat* inCat) override;
+
+	virtual uint32_t	GetAllStateMask()	const override { return EMRS_AllState; }
+
+	virtual uint32_t	Write(OutputMemoryBitStream& inOutputStream, uint32_t inDirtyState) const override;
+	virtual void		Read(InputMemoryBitStream& inInputStream) override;
 
 protected:
 	Platform();
